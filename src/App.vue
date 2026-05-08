1f41ec2a27eaea2bed0f3a2a96cf680339af5af3
Carlos.Arroyo <carlos23_adrian@hotmail.com>
2026-05-08 10:52:55 -0600

    modificacion de estructura

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <script setup lang="ts"></script>
 
-<template>
-  <div class="min-h-screen flex items-center justify-center">
-    <button class="btn btn-primary">Hola DaisyUI</button>
-  </div>
-
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
-</template>
+<template></template>
 
 <style scoped></style>
